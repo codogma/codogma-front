92c8849058a1b5430671652869d9e174f2252258
endzhe23 <endzhe.yarullina@gmail.com>
2024-06-23 14:08:24 +0300

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,19 +4,6 @@ import {createTheme} from '@mui/material/styles';
 import { Roboto } from 'next/font/google';
 import {Head} from "next/document";
 
-const roboto = Roboto({
-    weight: ['300', '400', '500', '700'],
-    subsets: ['latin'],
-    display: 'swap',
-    });
-
-const theme = createTheme({
-    typography: {
-    fontFamily: roboto.style.fontFamily,
-    },
-});
-
-
 export default function MyApp(props: JSX.IntrinsicAttributes & EmotionCacheProviderProps & { children?: ReactNode | undefined; }) {
     return (
          <AppCacheProvider {...props}>
