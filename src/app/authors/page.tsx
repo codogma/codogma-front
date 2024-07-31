1a1bf3b03108c2dc1f954a2db7f0e3114851a37f
endzhe23 <endzhe.yarullina@gmail.com>
2024-07-31 16:38:55 +0300

    revert users path

--- src/app/authors/page.tsx
+++ src/app/authors/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, {MouseEvent, useEffect, useState} from "react";
 import {User} from "@/types";
-import {deleteUser, getAuthors, getUsers} from "@/helpers/userApi";
+import {deleteUser, getUsers} from "@/helpers/userApi";
 import Link from "next/link";
 import {Button} from "@mui/material";
 
@@ -11,7 +11,7 @@ function Page() {
     useEffect(() => {
         async function fetchData() {
             try {
-                const allUsers = await getAuthors();
+                const allUsers = await getUsers();
                 console.log(allUsers)
                 setUsers(allUsers);
             } catch (error) {
