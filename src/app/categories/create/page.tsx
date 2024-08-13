38dbec9254797441d58f7f618fa73072062362b8
endzhe23 <endzhe.yarullina@gmail.com>
2024-08-13 16:51:45 +0300

    fix loading image

--- src/app/categories/create/page.tsx
+++ src/app/categories/create/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import "../../globals.css"
-import {z} from "zod"
+import "../../globals.css";
+import {z} from "zod";
 import {FormProvider, useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
 import React, {useEffect, useState} from "react";
@@ -11,13 +11,16 @@ import {WithAuth} from "@/components/WithAuth";
 import IconButton from "@mui/material/IconButton";
 import {ModeEditOutlineOutlined} from "@mui/icons-material";
 import {styled} from "@mui/material/styles";
-import {Category} from "@/types";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 
 const CategoryScheme = z.object({
-    name: z.string().min(2, "Название категории не может содержать менее 2 символов.").max(50, "Название категории не может содержать более 50 символов."),
+    name: z
+        .string()
+        .min(2, "Название категории не может содержать менее 2 символов.")
+        .max(50, "Название категории не может содержать более 50 символов."),
     image: z.optional(z.instanceof(File)),
-    description: z.optional(z.string())
-})
+    description: z.optional(z.string()),
+});
 
 const VisuallyHiddenInput = styled("input")({
     clip: "rect(0 0 0 0)",
@@ -31,9 +34,8 @@ const VisuallyHiddenInput = styled("input")({
     width: 1,
 });
 
-function Categoties() {
-    const [category, setCategory] = useState<Category>();
-    const [imageFile, setImageFile] = useState<File>();
+function Categories() {
+    const [imageFile, setImageFile] = useState<string>();
 
     const zodForm = useForm<z.infer<typeof CategoryScheme>>({
         resolver: zodResolver(CategoryScheme),
@@ -41,41 +43,35 @@ function Categoties() {
             name: "",
             image: undefined,
             description: "",
-        }
-    })
+        },
+    });
 
     const {
         reset,
         handleSubmit,
         register,
-        formState: {isSubmitSuccessful, errors}
-    } = zodForm
+        setValue,
+        formState: {isSubmitSuccessful, errors},
+    } = zodForm;
 
     useEffect(() => {
-        zodForm.reset({
-            name: category?.name,
-            image: undefined,
-            description: category?.description,
-        })
         if (isSubmitSuccessful) {
             reset(zodForm.getValues());
         }
-    }, [category?.description, category?.name, isSubmitSuccessful, reset, zodForm]);
+    }, [isSubmitSuccessful, reset, zodForm]);
 
     const onSubmit = (formData: z.infer<typeof CategoryScheme>) => {
-        const requestData = {...formData, image: imageFile}
-        console.log(formData)
-        createCategory(requestData)
-    }
+        console.log(formData);
+        createCategory(formData);
+    };
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
         if (file) {
-            setImageFile(file);
-            setCategory(prev => prev ? {...prev, imageUrl: URL.createObjectURL(file)} : prev);
+            setValue("image", file);
+            setImageFile(URL.createObjectURL(file));
         }
     };
-
 
     return (
         <main className="flex min-h-screen max-w-3xl flex-col items-left justify-self-auto p-24">
@@ -84,7 +80,8 @@ function Categoties() {
                     component="form"
                     noValidate
                     sx={{
-                        m: 1, width: '25ch',
+                        m: 1,
+                        width: "25ch",
                     }}
                     autoComplete="off"
                     onSubmit={handleSubmit(onSubmit)}
@@ -98,17 +95,21 @@ function Categoties() {
                                 <ModeEditOutlineOutlined color="primary"/>
                                 <VisuallyHiddenInput
                                     id="image"
-                                    name="image"
                                     type="file"
+                                    {...register("image")}
                                     onChange={handleFileChange}
                                 />
                             </IconButton>
                         }
                     >
-                        <Avatar className="bg-mountain-mist" variant="rounded" src={category?.imageUrl}
-                                sx={{width: 112, height: 112}}/>
-                            {/*<AddPhotoAlternateIcon/>*/}
-                        {/*</Avatar>*/}
+                        <Avatar
+                            className="bg-mountain-mist"
+                            variant="rounded"
+                            src={imageFile}
+                            sx={{width: 112, height: 112}}
+                        >
+                            <AddPhotoAlternateIcon/>
+                        </Avatar>
                     </Badge>
                     <FormInput name="description" label="Description" variant="standard"/>
                     <Button type="submit">Create</Button>
@@ -118,4 +119,4 @@ function Categoties() {
     );
 }
 
-export default WithAuth(Categoties)
+export default WithAuth(Categories);
