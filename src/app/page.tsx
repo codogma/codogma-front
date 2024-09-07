b7f1b46028ee8722149d56a12a7d82e140771367
endzhe23 <endzhe.yarullina@gmail.com>
2024-09-07 18:25:29 +0300

    add navTabs to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,38 @@
 
 import Typography from "@mui/material/Typography";
 import Grid from '@mui/material/Grid2';
+import NavTabs, {LinkTabProps} from "@/components/NavTabs";
+import React from "react";
 
 export default function Page() {
+
+
+    const tabs: LinkTabProps[] = [
+        {label: 'History', href: `/`},
+        {label: 'Bookmarks', href: `/`},
+        {label: 'Subscriptions', href: `/`}
+    ];
+
+
     return (
         <section>
             <section className="your-interest">
-                <Typography variant="h3" className="your-interest-h3">Ваши интересы</Typography>
-
+                <Typography variant="h3" className="your-interest-h3">Your interest</Typography>
+                <NavTabs tabs={tabs}/>
             </section>
             <section className="carousels-section">
                 <section className="recent-added">
                     <Grid container spacing={2}>
                         <Grid size={8}>
-                            <Typography variant="h3">Недавно добавлено</Typography>
+                            <Typography variant="h3">Recently added</Typography>
                         </Grid>
                     </Grid>
                 </section>
                 <section className="recommended">
-                    <Typography variant="h3">Рекомендуется для вас</Typography>
+                    <Typography variant="h3">Recommended for you</Typography>
                 </section>
                 <section className="popular">
-                    <Typography variant="h3">Самое популярное</Typography>
+                    <Typography variant="h3">Most popular</Typography>
                 </section>
             </section>
         </section>
