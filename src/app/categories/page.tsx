d81a90b53d832e7646089a6420ed7d5030862434
endzhe23 <endzhe.yarullina@gmail.com>
2024-09-09 21:49:27 +0300

    add skeleton for categories page

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -3,17 +3,13 @@ import React, {useEffect, useState} from "react";
 import {Category, UserRole} from "@/types";
 import {getCategories} from "@/helpers/categoryApi";
 import Link from "next/link";
-import {Badge, Button} from "@mui/material";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Stack from "@mui/material/Stack";
-import CardActions from "@mui/material/CardActions";
+import {Badge, Button, Card, CardActions, CardContent, IconButton, Skeleton, Stack,} from "@mui/material";
 import {useAuth} from "@/components/AuthProvider";
-import IconButton from "@mui/material/IconButton";
 import {AvatarImage} from "@/components/AvatarImage";
 
 export default function Page() {
-    const [categories, setCategories] = useState<Category[]>([])
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [loading, setLoading] = useState(true); // Состояние загрузки
     const {state} = useAuth();
 
     useEffect(() => {
@@ -22,56 +18,106 @@ export default function Page() {
                 const allCategories = await getCategories();
                 setCategories(allCategories);
             } catch (error) {
-                console.error('Error fetching data:', error)
+                console.error("Error fetching data:", error);
+            } finally {
+                setLoading(false); // Окончание загрузки данных
             }
         }
 
-        fetchData()
-    }, [])
+        fetchData();
+    }, []);
 
     return (
         <section>
-            {categories.map((category) => (
-                <Card key={category.id} variant="outlined" className="card">
+            {loading ? (
+                // Скелетоны для состояния загрузки
+                <Card variant="outlined" className="card">
                     <CardContent className="card-content">
                         <div className="meta-container">
-                            <Badge className="items-start"
-                                   overlap="circular"
-                                   anchorOrigin={{vertical: "bottom", horizontal: "right"}}
-                                   badgeContent={
-                                       <IconButton component="label" color="inherit" sx={{p: 0}}/>
-                                   }
-                            >
-                                <AvatarImage alt={category.name} className="category-img" variant="rounded"
-                                             src={category.imageUrl} size={48}/>
-                            </Badge>
+                            <Skeleton variant="circular" width={48} height={48}/>
                             <ul>
-                                <li><Link href={`/categories/${category.id}`}
-                                          className="category-name">{category.name}</Link>
-                                </li>
-                                <li><p className="category-description">{category.description}</p></li>
                                 <li>
-                                    <div className="category-tags">{category.tags?.map((tag) => (
-                                        <span className="tag-item" key={tag.id}>
-                                        <Link key={tag.id} href={`/tags/${tag.id}`}
-                                              className="tag-name">{tag.name}</Link>
-                                        </span>
-                                    ))}</div>
+                                    <Skeleton animation="wave" height={10} width="60%"/>
+                                </li>
+                                <li>
+                                    <Skeleton animation="wave" height={10} width="80%"/>
+                                </li>
+                                <li>
+                                    <Skeleton animation="wave" height={10} width="50%"/>
                                 </li>
                             </ul>
                         </div>
                         <CardActions className="p-0 m-0">
-                            <Stack direction="row" spacing={2}>
-                                {state.user?.role === UserRole.ROLE_ADMIN && (
-                                    <Link href={`/categories/edit/${category.id}`}>
-                                        <Button className="article-btn" variant="outlined">Update</Button>
-                                    </Link>
-                                )}
-                            </Stack>
+                            <Skeleton animation="wave" height={36} width="20%"/>
                         </CardActions>
                     </CardContent>
                 </Card>
-            ))}
+            ) : (
+                // Отображение категорий после загрузки данных
+                categories.map((category) => (
+                    <Card key={category.id} variant="outlined" className="card">
+                        <CardContent className="card-content">
+                            <div className="meta-container">
+                                <Badge
+                                    className="items-start"
+                                    overlap="circular"
+                                    anchorOrigin={{vertical: "bottom", horizontal: "right"}}
+                                    badgeContent={
+                                        <IconButton component="label" color="inherit" sx={{p: 0}}/>
+                                    }
+                                >
+                                    <AvatarImage
+                                        alt={category.name}
+                                        className="category-img"
+                                        variant="rounded"
+                                        src={category.imageUrl}
+                                        size={48}
+                                    />
+                                </Badge>
+                                <ul>
+                                    <li>
+                                        <Link
+                                            href={`/categories/${category.id}`}
+                                            className="category-name"
+                                        >
+                                            {category.name}
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <p className="category-description">{category.description}</p>
+                                    </li>
+                                    <li>
+                                        <div className="category-tags">
+                                            {category.tags?.map((tag) => (
+                                                <span className="tag-item" key={tag.id}>
+                          <Link
+                              key={tag.id}
+                              href={`/tags/${tag.id}`}
+                              className="tag-name"
+                          >
+                            {tag.name}
+                          </Link>
+                        </span>
+                                            ))}
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <CardActions className="p-0 m-0">
+                                <Stack direction="row" spacing={2}>
+                                    {state.user?.role === UserRole.ROLE_ADMIN && (
+                                        <Link href={`/categories/edit/${category.id}`}>
+                                            <Button className="article-btn" variant="outlined">
+                                                Update
+                                            </Button>
+                                        </Link>
+                                    )}
+                                </Stack>
+                            </CardActions>
+                        </CardContent>
+                    </Card>
+                ))
+            )}
         </section>
     );
 }
