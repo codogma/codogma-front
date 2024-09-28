7e5e78be9b6186ca408f7238893681526da5049b
endzhe23 <endzhe.yarullina@gmail.com>
2024-09-28 16:13:22 +0300

    delete use client from CommentForm component

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -1,4 +1,3 @@
-'use client';
 import React, { useEffect, useState } from 'react';
 import { CreateComment, GetComment, UpdateComment } from '@/types';
 import { createComment, updateComment } from '@/helpers/commentAPI';
