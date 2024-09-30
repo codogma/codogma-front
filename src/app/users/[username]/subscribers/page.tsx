7c367c1f593b8a0c46d53d4d63262d47086730e2
endzhe23 <endzhe.yarullina@gmail.com>
2024-09-30 14:26:24 +0300

    add skeleton for subscriptions and subscribers

--- src/app/users/[username]/subscribers/page.tsx
+++ src/app/users/[username]/subscribers/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { User } from '@/types';
-import { getUserByUsername } from '@/helpers/userApi';
 import Link from 'next/link';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import { Box } from '@mui/material';
+import { Box, Skeleton } from '@mui/material';
 import { AvatarImage } from '@/components/AvatarImage';
+import { getUserByUsername } from '@/helpers/userApi';
 
 type PageParams = {
   username: string;
@@ -19,12 +19,14 @@ type PageProps = {
 const Page = ({ params }: PageProps) => {
   const username: string = params.username;
   const [subscribers, setSubscribers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const user = await getUserByUsername(username);
         setSubscribers(user.subscribers);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -33,30 +35,47 @@ const Page = ({ params }: PageProps) => {
     fetchData();
   }, [username]);
 
-  return subscribers?.map((user) => (
-    <Card key={user.username} variant='outlined' className='card'>
-      <CardContent className='card-content'>
-        <Box className='meta-container'>
-          <>
-            <AvatarImage
-              className='article-user-avatar'
-              src={user.avatarUrl}
-              alt={user.username}
-              variant='rounded'
-              size={32}
-            />
-            <Link
-              href={`/users/${user.username}`}
-              className='subscribers-user-name'
-            >
-              @{user.username}
-            </Link>
-          </>
-          <div className='subscribers-user-description'>{user.shortInfo}</div>
-        </Box>
-      </CardContent>
-    </Card>
-  ));
+  return (
+    <>
+      {loading ? (
+        <Card className='card'>
+          <CardContent className='card-content'>
+            <Box className='meta-container'>
+              <Skeleton variant='rounded' width={32} height={32} />
+              <Skeleton variant='text' width={100} />
+            </Box>
+          </CardContent>
+        </Card>
+      ) : (
+        subscribers?.map((user) => (
+          <Card key={user.username} variant='outlined' className='card'>
+            <CardContent className='card-content'>
+              <Box className='meta-container'>
+                <>
+                  <AvatarImage
+                    className='article-user-avatar'
+                    src={user.avatarUrl}
+                    alt={user.username}
+                    variant='rounded'
+                    size={32}
+                  />
+                  <Link
+                    href={`/users/${user.username}`}
+                    className='subscribers-user-name'
+                  >
+                    @{user.username}
+                  </Link>
+                </>
+                <div className='subscribers-user-description'>
+                  {user.shortInfo}
+                </div>
+              </Box>
+            </CardContent>
+          </Card>
+        ))
+      )}
+    </>
+  );
 };
 
 export default Page;
--- src/app/users/[username]/subscriptions/page.tsx
+++ src/app/users/[username]/subscriptions/page.tsx
@@ -5,7 +5,7 @@ import { getUserByUsername } from '@/helpers/userApi';
 import Link from 'next/link';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import { Box } from '@mui/material';
+import { Box, Skeleton } from '@mui/material';
 import { AvatarImage } from '@/components/AvatarImage';
 
 type PageParams = {
@@ -19,12 +19,14 @@ type PageProps = {
 const Page = ({ params }: PageProps) => {
   const username: string = params.username;
   const [subscriptions, setSubscriptions] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const user = await getUserByUsername(username);
         setSubscriptions(user.subscriptions);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -33,30 +35,47 @@ const Page = ({ params }: PageProps) => {
     fetchData();
   }, [username]);
 
-  return subscriptions?.map((user) => (
-    <Card key={user.username} variant='outlined' className='card'>
-      <CardContent className='card-content'>
-        <Box className='meta-container'>
-          <>
-            <AvatarImage
-              className='article-user-avatar'
-              src={user.avatarUrl}
-              alt={user.username}
-              variant='rounded'
-              size={32}
-            />
-            <Link
-              href={`/users/${user.username}`}
-              className='subscriptions-user-name'
-            >
-              @{user.username}
-            </Link>
-          </>
-          <div className='subscriptions-user-description'>{user.shortInfo}</div>
-        </Box>
-      </CardContent>
-    </Card>
-  ));
+  return (
+    <>
+      {loading ? (
+        <Card className='card'>
+          <CardContent className='card-content'>
+            <Box className='meta-container'>
+              <Skeleton variant='rounded' width={32} height={32} />
+              <Skeleton variant='text' width={100} />
+            </Box>
+          </CardContent>
+        </Card>
+      ) : (
+        subscriptions?.map((user) => (
+          <Card key={user.username} variant='outlined' className='card'>
+            <CardContent className='card-content'>
+              <Box className='meta-container'>
+                <>
+                  <AvatarImage
+                    className='article-user-avatar'
+                    src={user.avatarUrl}
+                    alt={user.username}
+                    variant='rounded'
+                    size={32}
+                  />
+                  <Link
+                    href={`/users/${user.username}`}
+                    className='subscriptions-user-name'
+                  >
+                    @{user.username}
+                  </Link>
+                </>
+                <div className='subscriptions-user-description'>
+                  {user.shortInfo}
+                </div>
+              </Box>
+            </CardContent>
+          </Card>
+        ))
+      )}
+    </>
+  );
 };
 
 export default Page;
