e8e4c11914e66b3d5cda545de63995743a06b4bb
endzhe23 <endzhe.yarullina@gmail.com>
2024-11-17 13:58:25 +0300

    change styles

--- src/app/[lng]/articles/page.tsx
+++ src/app/[lng]/articles/page.tsx
@@ -139,7 +139,6 @@ export default function Page({ params: { lng } }: PageProps) {
           position: 'sticky',
           top: '64px',
           zIndex: 10,
-          backgroundColor: 'white',
           boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
           p: '6px',
           m: '0px auto 8px auto',
